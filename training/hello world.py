0f37836118df409cfe814733362bb53d192d1362
Sana Jamal <sana.jamal@sky.uk>
2024-05-29 14:57:46 +0100

    training foundation

---
+++ training/hello world.py
@@ -0,0 +1,8 @@
+my_age = 23
+print(my_age)
+half_my_age = my_age/2
+print(half_my_age)
+greeting = "hello"
+name = " my name is sana"
+greeting_with_name = greeting + name
+print(greeting_with_name)
